939c69f93815a2bee994ad65862f6ddfcac5cef1
Alisson Costa <alisson.franciscocosta@gmail.com>
2021-11-26 02:32:11 -0300

    Finalização do crud, relação Many To Many, One To Many, Dashboard e participação de eventos.

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth'); //direcionar para página create
 Route::get('/events/{id}', [EventController::class, 'show']); //rota para mostrar evento
 Route::post('/events', [EventController::class, 'store']); //criar dados no banco
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');;
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');; 
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth');; 
 
 Route::get('/contact', function () {
     return view('contact');
@@ -26,3 +29,5 @@ Route::get('/contact', function () {
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
+Route::post('/events/join/{id}', [EventController::class, 'JoinEvent']); //criar dados no banco
+Route::delete('/events/leave/{id}', [EventController::class, 'LeaveEvent']); //criar dados no banco
